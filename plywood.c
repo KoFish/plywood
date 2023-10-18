80e4bfe1b77d19775195130fb9ab25ba8abd0a00
Krister Svanlund <kofish@mailbox.org>
2023-10-19 01:01:25 +0200

    Added back missing oled graphics

--- plywood.c
+++ plywood.c
@@ -1,20 +1,42 @@
 #include QMK_KEYBOARD_H
+#include "plywood.h"
 
-enum layers {
-    _BASE,
-    _EXT,
-    _FUN,
-    _SYM,
-    _NUM,
-    _FIVE,
-    _SIX,
-};
 
 #ifdef OLED_ENABLE
 #   include "plywood_oled.h"
 
 bool oled_task_user(void) {
-    render_layer_state();
+    render_logo();
+    render_space();
+    render_plywood_layer_indicator();
+    render_mod_status_gui_alt(get_mods()|get_oneshot_mods());
+    render_mod_status_ctrl_shift(get_mods()|get_oneshot_mods());
+    render_kb_LED_state();
     return false;
 }
-#endif
+#endif // OLED
+
+#ifdef CAPS_WORD_ENABLE
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case RALT(KC_W):
+        case RALT(KC_P):
+        case RALT(KC_Q):
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+#endif // CAPS_WORDS
--- plywood_oled.c
+++ plywood_oled.c
@@ -1,6 +1,43 @@
+#include QMK_KEYBOARD_H
 #include "plywood_oled.h"
 
-void render_layer_state(void) {
+// LAYER OFF --------------------------------------------------------------------------------------
+
+static const char PROGMEM LAY_TOP_LEFT_OFF_1[] = {_TOP_LEFT_EMPTY_1, _TOP_EMPTY, 0};
+static const char PROGMEM LAY_TOP_RIGHT_OFF_1[] = {_TOP_EMPTY, _TOP_RIGHT_EMPTY_1, 0};
+static const char PROGMEM LAY_TOP_LEFT_OFF_2[] = {_TOP_LEFT_EMPTY_2, _TOP_LEFT_EMPTY_3, 0};
+static const char PROGMEM LAY_TOP_RIGHT_OFF_2[] = {_TOP_RIGHT_EMPTY_2, _TOP_RIGHT_EMPTY_3, 0};
+
+static const char PROGMEM LAY_MID_LEFT_OFF_1[] = {_TOP_LEFT_EMPTY_1, _MID_LEFT_EMPTY, 0};
+static const char PROGMEM LAY_MID_RIGHT_OFF_1[] = {_MID_RIGHT_EMPTY, _TOP_RIGHT_EMPTY_1, 0};
+static const char PROGMEM LAY_MID_LEFT_OFF_2[] = {_TOP_LEFT_EMPTY_2, _TOP_LEFT_EMPTY_3, 0};
+static const char PROGMEM LAY_MID_RIGHT_OFF_2[] = {_TOP_RIGHT_EMPTY_2, _TOP_RIGHT_EMPTY_3, 0};
+
+static const char PROGMEM LAY_BOTTOM_LEFT_OFF_1[] = {_TOP_LEFT_EMPTY_1, _MID_LEFT_EMPTY, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_OFF_1[] = {_MID_RIGHT_EMPTY, _TOP_RIGHT_EMPTY_1, 0};
+static const char PROGMEM LAY_BOTTOM_LEFT_OFF_2[] = {_TOP_LEFT_EMPTY_2, _BOTTOM_EMPTY, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_OFF_2[] = {_BOTTOM_EMPTY, _TOP_RIGHT_EMPTY_3, 0};
+
+// LAYER ON ---------------------------------------------------------------------------------------
+
+static const char PROGMEM LAY_TOP_LEFT_ON_1[] = {_TOP_LEFT_FULL_1, _TOP_LEFT_FULL_3, 0};
+static const char PROGMEM LAY_TOP_RIGHT_ON_1[] = {_TOP_RIGHT_FULL_1, _TOP_RIGHT_FULL_3, 0};
+static const char PROGMEM LAY_TOP_LEFT_ON_2[] = {_TOP_LEFT_FULL_2, _TOP_LEFT_FULL_4, 0};
+static const char PROGMEM LAY_TOP_RIGHT_ON_2[] = {_TOP_RIGHT_FULL_2, _TOP_RIGHT_FULL_4, 0};
+
+static const char PROGMEM LAY_MID_LEFT_ON_1[] = {_TOP_LEFT_FULL_1, _MID_LEFT_FULL, 0};
+static const char PROGMEM LAY_MID_RIGHT_ON_1[] = {_MID_RIGHT_FULL_1, _MID_RIGHT_FULL_2, 0};
+static const char PROGMEM LAY_MID_LEFT_ON_2[] = {_TOP_LEFT_FULL_2, _TOP_LEFT_FULL_4, 0};
+static const char PROGMEM LAY_MID_RIGHT_ON_2[] = {_TOP_RIGHT_FULL_2, _TOP_RIGHT_FULL_4, 0};
+
+static const char PROGMEM LAY_BOTTOM_LEFT_ON_1[] = {_TOP_LEFT_FULL_1, _MID_LEFT_FULL, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_ON_1[] = {_MID_RIGHT_FULL_1, _TOP_RIGHT_FULL_1, 0};
+static const char PROGMEM LAY_BOTTOM_LEFT_ON_2[] = {_BOTTOM_LEFT_FULL, _BOTTOM_FULL_1, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_ON_2[] = {_BOTTOM_FULL_2, _TOP_RIGHT_FULL_3, 0};
+
+// ------------------------------------------------------------------------------------------------
+
+void render_plywood_layer_indicator(void) {
     oled_write_P(IS_LAYER_ON(_EXT) ? LAY_TOP_LEFT_ON_1 : LAY_TOP_LEFT_OFF_1, false);
     oled_write_char(_MID_1, false);
     oled_write_P(IS_LAYER_ON(_FUN) ? LAY_TOP_RIGHT_ON_1 : LAY_TOP_RIGHT_OFF_1, false);
@@ -22,4 +59,4 @@ void render_layer_state(void) {
     oled_write_char(_MID_4, false);
     oled_write_P(IS_LAYER_ON(_SIX) ? LAY_BOTTOM_RIGHT_ON_2 : LAY_BOTTOM_RIGHT_OFF_2, false);
 }
-}
+
